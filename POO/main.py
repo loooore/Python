0df633fb3fb786ac85c5f1495a98efed86ecbd69
lore <lorenzo.toledo789@gmail.com>
2024-04-19 08:55:44 -0300

    Estudo de POO

--- POO/main.py
+++ POO/main.py
@@ -1,6 +1,43 @@
 from pessoa import Pessoa
 
-p1 = Pessoa()
-p2 = Pessoa()
+p1 = Pessoa('Lorenzo', 19)
+p1.comer('maçã')
+p1.comer('maçã')
+p1.parar_comer()
+p1.parar_comer()
+p1.comer(1)
+print()
 
-print
+p1.falar('Olá')
+p1.parar_comer()
+p1.falar('Olá galera')
+p1.comer('cacto')
+p1.parar_falar()
+p1.comer('vidro')
+
+print()
+print('-'*100)
+print()
+
+p2 = Pessoa('Lucca', 21)
+p3 = Pessoa('Arthur', 18)
+
+p2.falar('Sou burro')
+p3.falar('É msm')
+
+p2.comer('vidro')
+p3.parar_falar()
+p3.comer('bolacha')
+
+print()
+print('-'*100)
+print()
+
+print(p1.ano_atual)
+print(Pessoa.ano_atual)
+
+print()
+print('-'*100)
+print()
+
+print(f'{p1.nome} nasceu em {p1.get_ano_nascimento()}')
--- POO/pessoa.py
+++ POO/pessoa.py
@@ -1,2 +1,59 @@
+from datetime import datetime
+
+
 class Pessoa:
-    pass
+    ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
+
+    """
+        __init__ - É o objeto construtor, utilizado para definir os atributos de um objeto
+        self : Acessar os atributos e métodos do objeto usando self.nome_do_atributo ou self.nome_do_metodo().
+               Torna as variáveis acessíveis em todo os objetos
+    """
+
+    def __init__(self, nome, idade, comendo=False, falando=False):
+        self.nome = nome
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+
+    def falar(self, assunto):
+        if self.comendo:
+            print(f'{self.nome}, não pode falar comendo.')
+            return
+
+        if self.falando:
+            print(f'{self.nome}, já está falando.')
+
+        print(f'{self.nome}, está falando: {assunto}')
+        self.falando = True
+
+    def parar_falar(self):
+        if not self.falando:
+            print(f'{self.nome}, não está falando.')
+            return
+
+        print(f'{self.nome}, parou de falar')
+        self.falando = False
+
+    def comer(self, alimento):
+        if self.comendo:
+            print(f'{self.nome}, já está comendo.')
+            return  # Para a função aqui
+
+        if self.falando:
+            print(f'{self.nome}, não pode comer falando.')
+            return
+
+        print(f'{self.nome}, está comendo {alimento}')
+        self.comendo = True
+
+    def parar_comer(self):
+        if not self.comendo:
+            print(f'{self.nome} não está comendo.')
+            return
+
+        print(f'{self.nome}, parou de comer.')
+        self.comendo = False
+
+    def get_ano_nascimento(self):
+        return self.ano_atual - self.idade
